ceb83713b5ca443033236de8d50de53e24542fbc
Mark Yasenskiy <mark.ositis@gmail.com>
2023-05-01 19:35:13 +0300

    first commit in IDEA

---
+++ src/Start.java
@@ -0,0 +1,11 @@
+package src;
+
+public class Start {
+    public static void main(String[] args) {
+
+        int num1 = 1;
+        int num2 = 5;
+        int result = num1 + num2;
+        System.out.println(result);
+    }
+}
